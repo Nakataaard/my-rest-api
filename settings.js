fef985c72adc42837694a0ec93edd6e2a365b568
Nakataaard <98439130+Nakataaard@users.noreply.github.com>
2023-08-05 16:12:52 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.creator = 'Nakata Ardinata'// yourname
+global.MONGO_DB_URI = "mongodb+srv://nahbot:mahavalen1@cluster0.sapfqfb.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "valencianakata1447@gmail.com" //email
+global.email_password = "mahavalen1" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
